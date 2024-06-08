5a5b7b2d32c401520dc347e4aefa24ed43824386
zbalogh <zbal1977@gmail.com>
2024-06-08 16:48:04 +0200

    added script to create docker-data directories

---
+++ create-docker-data-dirs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ./docker-data/postgres-data
+mkdir -p ./docker-data/mongo-data
+mkdir -p ./docker-data/redis-data
+mkdir -p ./docker-data/keycloak-data
+mkdir -p ./docker-data/prometheus-data
+mkdir -p ./docker-data/grafana-data
